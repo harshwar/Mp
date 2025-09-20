932bf76f228760c7e31278731b13147c77c1c52d
Harshwardhan Chauhan <harsh15022003@gmail.com>
2025-09-20 06:48:39 +0530

    Update index.js

--- Cordova/P11/index.js
+++ Cordova/P11/index.js
@@ -6,7 +6,7 @@ function addNumbers() {
     var num1 = parseFloat(document.getElementById('num1').value);
     var num2 = parseFloat(document.getElementById('num2').value);
 
-    // Check if the inputs are valid numbers
+    
     if (isNaN(num1) || isNaN(num2)) {
         document.getElementById('result').innerText = "Please enter valid numbers.";
     } else {
