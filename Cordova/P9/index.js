a92e5b09ba85d83fd71fece0739c4865dd16b039
Harshwardhan Chauhan <harsh15022003@gmail.com>
2025-09-20 06:49:04 +0530

    Update index.js

--- Cordova/P9/index.js
+++ Cordova/P9/index.js
@@ -3,5 +3,5 @@ function onDeviceReady() {
     alert('Hello World Harshwardhan KCTYBSCIT06');
 }
 
-// Wait for Cordova to connect with the device
+
 document.addEventListener('deviceready', onDeviceReady, false);
