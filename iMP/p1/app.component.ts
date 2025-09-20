5583a1244a65224b7ebd75f36845ee7361592b73
Harshwardhan Chauhan <harsh15022003@gmail.com>
2025-09-20 07:01:47 +0530

    Create app.component.ts

---
+++ iMP/p1/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+@Component({
+ selector: 'app-root',
+ standalone: true,
+ imports: [IonicModule, CommonModule],
+ template: `<ion-app><ion-router-outlet></ion-router-outlet></ion-app>`,
+})
+export class AppComponent {}
