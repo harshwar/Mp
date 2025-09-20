40562c308728d370572ead326bf2a419d3fb04fd
Harshwardhan Chauhan <harsh15022003@gmail.com>
2025-09-20 06:47:21 +0530

    Update home.page.ts

--- iMP/p2/home.page.ts
+++ iMP/p2/home.page.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-// Note: No need to import other modules in standalone components for this logic
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -14,8 +12,8 @@ export class HomePage {
   constructor(private router: Router) {}
 
   login() {
-    // Simple, direct validation
-    if (this.username === 'isha' && this.password === 'isha123') {
+    
+    if (this.username === 'harsh' && this.password === 'harsh123') {
       this.router.navigate(['/dashboard'], { state: { user: this.username } });
     } else {
       this.error = 'Invalid credentials';
