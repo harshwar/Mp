6aa47d097b7f8bd6e4ea364c0b692a733c6a9a47
Harshwardhan Chauhan <harsh15022003@gmail.com>
2025-09-20 07:03:35 +0530

    Update home.page.ts

--- iMP/p2/home.page.ts
+++ iMP/p2/home.page.ts
@@ -1,22 +1,40 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
+ selector: 'app-home',
+ standalone: true,
+ imports: [IonicModule, FormsModule, CommonModule],
+ templateUrl: './home.page.html',
+ styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-  username = '';
-  password = '';
-  error = '';
-
-  constructor(private router: Router) {}
-
-  login() {
-    
-    if (this.username === 'harsh' && this.password === 'harsh123') {
-      this.router.navigate(['/dashboard'], { state: { user: this.username } });
-    } else {
-      this.error = 'Invalid credentials';
-    }
-  }
+ username = '';
+ password = '';
+ loginMessage = '';
+ counter = 0;
+ constructor(private router: Router) {}
+ login() {
+if (this.username === 'ishita' && this.password === 'ishita123') {
+ this.loginMessage = 'Login Successful';
+ // Navigate to Student page and send demo data
+ this.router.navigate(['/student'], {
+ state: {
+ name: 'Ishita',
+ email: 'ishita@example.com',
+ dob: '2005-06-30',
+city: 'Mumbai',
+ gender: 'Female',
+ hobbies: { reading: true, sports: false, music: true }
+ }
+ });
+ } else {
+ this.loginMessage = 'Invalid username/password';
+ }
+ }
+ increaseCounter() {
+ this.counter++;
+ }
 }
